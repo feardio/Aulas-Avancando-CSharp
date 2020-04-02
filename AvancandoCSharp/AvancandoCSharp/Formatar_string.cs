f605575fcb6ce7403153d017d3abb2ec9c3488d0
Vinicius Valencise <feardio@gmail.com>
2020-04-02 16:56:23 -0300

    Começando as aulas do modulo Avançado de C#

---
+++ AvancandoCSharp/AvancandoCSharp/Formatar_string.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace AvancandoCSharp
+{
+    class Formatar_string
+    {
+        public void Formatar(string[] args)
+        {
+
+            string nome = "Vinicius Valencise";
+            int idade = 19;
+            double rendimento = 1500.00;
+            DateTime dataCadastro = new DateTime(2015, 05, 20);
+
+            string str = "O cliente {0} tem {1} anos de idade e tem rendimento de {2:c}";
+            str += " e é cliente desde {3:dd/MM/yyyy}";
+            string frase = String.Format(str, nome, idade, rendimento, dataCadastro);
+
+            WriteLine(frase);
+
+        }
+    }
+}
---
+++ AvancandoCSharp/AvancandoCSharp/funcoes_string.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+using static System.Console;
+namespace AvancandoCSharp
+{
+    class funcoes_string
+    {
+        public void funcoes_strings(string[] args)
+        {
+
+            string frase = "Hoje à noite, sem luz, decidi xeretar a quinta gaveta de vovô: achei lingüiça, pão e fubá";
+
+            //string frase_trocada = frase.Replace("vovô", "mamãe");
+            string frase_trocada = frase.Replace("VOVô", "mamãe", true, CultureInfo.CurrentCulture);
+
+            WriteLine(frase_trocada);
+
+
+
+            //WriteLine(frase.ToUpper());
+            //WriteLine(frase.ToLower());
+
+
+            //string subFrase = "achei";
+            //int posInicial = frase.IndexOf(subFrase);
+            //int posFinal = 10;
+
+            //string sub = frase.Substring(posInicial);
+
+            //WriteLine(sub);
+
+
+            //int pos = frase.IndexOf(subFrase);
+            //WriteLine(pos);
+
+
+
+
+
+            //var achou = frase.Contains(subFrase);
+            //WriteLine(achou);
+
+            //WriteLine(frase.Length); //Contar quantos caracteres
+
+            //string[] partes = frase.Split(",");
+
+            //for (var i = 0; i < partes.Length; i++)
+            //{
+            //  WriteLine(partes[i].Trim());
+            //}
+
+
+
+        }
+    }
+}
